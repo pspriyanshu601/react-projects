a766a4d1bf2fd9608b644feb2322c43e0a5d7b7c
priyansh <priyanshusingh6014@gmail.com>
2024-03-04 10:16:57 +0530

    first commit

---
+++ kela.cpp
@@ -0,0 +1,28 @@
+#include<cstdio>
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<queue>
+#include<stack>
+#include<utility>
+#include<string>
+#include<cstring>
+#include<set>
+#include<vector>
+#include<fstream>
+#include<map>
+#include<list>
+#include<algorithm>
+using namespace std;
+#define int long long
+int mod=1000000007;
+int MAX_INT=2147483647;
+                      
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+cout<<"lol";
+return 0;
+}
