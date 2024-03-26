5452d7cb93fcb004237970c69bfc543be25a35a0
pspriyanshu601 <priyanshusingh6014@gmail.com>
2024-03-26 23:03:25 +0530

    frontend completed

--- animalFarm/src/App.jsx
+++ animalFarm/src/App.jsx
@@ -1,11 +1,41 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from "axios";
 
-function App() {
-
+function Animal({type, name, age}) { 
   return (
-   <div>
-   </div>
-  )
+    <li><strong>{type}</strong> {name} {age}</li>
+  );
 }
 
-export default App
+function App() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(()=>{
+    const lastQuery=localStorage.getItem('lastQuery');
+    search(lastQuery);
+
+  },[])
+
+  async function search(query) {
+    const { data } = await axios.get(`https://react-projects-five-chi.vercel.app/animals?q=${query}`);
+    console.log(data);
+    setAnimals(data); // Update state with the fetched data
+    localStorage.setItem('lastQuery',query);
+  }
+
+  return (
+    <main>
+      <h1>Animal Farm</h1>
+      <input type="text" placeholder='Enter name' onChange={(e) => search(e.target.value)} />
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal}></Animal>
+        ))}
+        {animals.length === 0 && <li>No animals found</li>}
+      </ul>
+    </main>
+  );
+}
+
+export default App;
