a8c309353b61196abc0ec112a7300e2455fbd328
pspriyanshu601 <priyanshusingh6014@gmail.com>
2024-03-26 07:42:04 +0530

    first commit

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,24 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const LoadingButton = (props) => {
-  return (
-    <button onClick={props.onClick} type="button">
-      {props.loading ? <div className="loader" /> : props.label}
-    </button>
-  );
+const Countdown=({hr,min,sec})=>{
+    const [over,setOver]=useState(false);
+    const [paused,setPaused]=useState(true);
+    const [[h,m,s],setTime]=useState([hr,min,sec]);
+    
+    const tick=()=>{
+      if(paused||over){
+        return ;
+      }
+      if(h===0 && m===0 && s===0){
+        setOver(true);
+      }
+      else if(m===0 && s===0){
+        setTime([h-1,59,59]);
+      }
+      else if(s===0){
+        setTime([h,m-1,59]);
+      }
+      else{
+        setTime([h,m,s-1]);
+      }
+    }
+    const handleReset = () => {
+      setTime([hr, min, sec]);
+      setPaused(true);
+      setOver(false);
+    };
+  
+    const handlePause = () => setPaused(!paused);
+  
+    const fmt = (val) => val.toString().padStart(2, '0');
+
+    useEffect(()=>{
+      let ticker=setInterval(() => tick(), 1000);
+      return ()=>{
+        clearInterval(ticker);
+      };
+    });
+    return (
+      <div>
+        <h3 className="countdown">{`${fmt(h)}:${fmt(m)}:${fmt(s)}`}</h3>
+        <button onClick={handlePause}>{paused ? 'Start' : 'Pause'}</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    );
+  
 };
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-
+  
   return (
-    <>
-      <LoadingButton
-        label="Press me"
-        loading={isLoading}
-        onClick={() => setIsLoading(!isLoading)}
-      />
-    </>
+    <div>
+      <Countdown hr={1} min={45} sec={0} />
+    </div>
   );
 }
 
