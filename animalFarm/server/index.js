7983317b1801efbb144785fb29da98d158e418d9
pspriyanshu601 <priyanshusingh6014@gmail.com>
2024-03-27 10:03:34 +0530

    lol

--- animalFarm/server/index.js
+++ animalFarm/server/index.js
@@ -5,11 +5,8 @@ import Chance from "chance"
 
 const app = express();
 
-const corsOptions = {
-    origin: 'https://react-projects-rdre-kj2xf94dx.vercel.app/',
-  };
   
-app.use(cors(corsOptions)); // Apply CORS middleware with the specified options
+app.use(cors()); // Apply CORS middleware with the specified options
 app.use(express.json());
 
 const port = 3000;
