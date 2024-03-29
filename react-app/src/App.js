63c07077ca87eb9cc637e5f90bcbc9137ae7fb06
pspriyanshu601 <priyanshusingh6014@gmail.com>
2024-03-29 18:50:37 +0530

    .gitignore is now working

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,16 @@
-import ErrorBoundary from "./compoenents.jsx/ErrorBoundary";
-
+import logo from "./logo.svg"
+import { useState } from "react";
+import "./App.css"
+import Navbar from "./components/navbar.jsx";
+ 
 function App() {
+  const [value,setValue]=useState(0);
   return (
-    <div>
-    <h3>Outside the error boundary</h3>
-    <ErrorBoundary>
-    <h3>This is inside error Boundary</h3>
-    </ErrorBoundary>
+    <div className="App">
+    Hey Harry {value}
+    <button onClick={()=>setValue(value+1)}> Increase count</button>
+    <button onClick={()=>setValue(0)}>Reset</button>
+    <Navbar></Navbar>
     </div>
   )
 }
---
+++ react-app/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+    <ul>
+        <li>Home</li>
+        <li>About us </li>
+        <li>Our products</li>
+    </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
